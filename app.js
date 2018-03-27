56ff66ea6d2da18e12731ac7a2dfa8675477b301
evanlewis1 <evanl2@student.unimelb.edu.au>
2018-03-27 18:06:32 +1100

    Added stylesheets

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 var express = require("express")
-var Users = require("./models/db")
-var ejs = require("ejs")
+var path = require('path');
 
 app = express()
 
-app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', function(req,res){
-    res.send("Hello World");
-});
-app.get('/bye', function(req,res){
-    res.send("Goodbye world");
+    res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get('/users', (req,res) => res.render("user_template.ejs"))
-app.listen();
+
+app.get('/public/stylesheets/comingsoon.css', function(req,res){
+    res.sendFile(__dirname + "/public/stylesheets/comingsoon.css");
+});
+
+app.get('/public/stylesheets/style.css', function(req,res){
+    res.sendFile(__dirname + "/public/stylesheets/style.css");
+});
+
+
+http://localhost:3000/public/stylesheets/comingsoon.css
+app.listen("3000");
